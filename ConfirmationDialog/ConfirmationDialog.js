f5531cf09caf2c9629ee103b32fa90eefa151479
Michał Legut <michal.legut@protonmail.com>
2021-10-21 23:13:47 +0200

    Implemented ConfirmationDialog buttons logic and connected them to main page, simplified component structure

--- ConfirmationDialog/ConfirmationDialog.js
+++ ConfirmationDialog/ConfirmationDialog.js
@@ -1,13 +1,11 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <link href="ConfirmationDialog/ConfirmationDialog.css" rel="stylesheet">
-    <div id="confirmation-dialog-backscreen">
-        <div class="confirmation-dialog">
-            <h3 id="confirmation-dialog-message"></h3>
-            <div class="confirmation-dialog-buttons-wrapper">
-                <button class="confirmation-dialog-button">Yes</button>
-                <button class="confirmation-dialog-button">Cancel</button>
-            </div>
+    <div class="confirmation-dialog">
+        <h3 id="confirmation-dialog-message"></h3>
+        <div class="confirmation-dialog-buttons-wrapper">
+            <button id="confirmation-dialog-yes-button" class="confirmation-dialog-button">Yes</button>
+            <button id="confirmation-dialog-cancel-button" class="confirmation-dialog-button">Cancel</button>
         </div>
     </div>
     `;
@@ -17,13 +15,23 @@ class ConfirmationDialog extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.querySelector("#confirmation-dialog-backscreen").style.display = 'none';
+        this.style.display = 'none';
     }
 
     display(message, callback) {
-        this.shadowRoot.querySelector("#confirmation-dialog-message").innerText = message;
+        this.shadowRoot.querySelector('#confirmation-dialog-message').innerText = message;
         this.callback = callback;
-        this.shadowRoot.querySelector("#confirmation-dialog-backscreen").style.display = 'flex';
+        this.style.display = 'flex';
+    }
+
+    handleButtonClick(event) {
+        this.style.display = 'none';
+        this.callback(event.target.innerText);
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector('#confirmation-dialog-yes-button').addEventListener('click', this.handleButtonClick.bind(this));
+        this.shadowRoot.querySelector('#confirmation-dialog-cancel-button').addEventListener('click', this.handleButtonClick.bind(this));
     }
 }
 
